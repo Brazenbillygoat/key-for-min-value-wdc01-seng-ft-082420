f0453b6f6958feff305e68fe6c804ca382dc72cf
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-10 18:29:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash == {}
-    return nil
-  end
+  p nil if name_hash == {}
   min_value = name_hash.first()
   
   
