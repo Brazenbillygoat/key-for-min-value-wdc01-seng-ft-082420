2345c9138aa2bcf3ae7e50cb8c55b1f60c8d7162
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-10 18:26:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,8 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |key, value|
     if value < min_value[1]
-      min_value = {key: value}
+      min_value = [key, value]
+    end
     
     
   end
