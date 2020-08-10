1e63e991fe40758f3920cf8178f694e52ec53101
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-10 18:14:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,15 @@
 
 def key_for_min_value(name_hash)
   p nil if name_hash = {}
-  min_value = name_hash
+  min_value = name_hash.first()
+  
   
   name_hash.each do |key, value|
-    
+    if value < min_value[1]
+      min_value = {key: value}
     
     
   end
   
-  
+  p min_value[0]
 end
